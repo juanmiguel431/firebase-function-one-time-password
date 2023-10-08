149c15d22761c7a946c37f87afdb1964037c802c
juanmiguel431 <juanmiguel431@gmail.com>
2023-10-08 15:13:09 -0400

    User creation implemented.

--- functions/src/createUser.ts
+++ functions/src/createUser.ts
@@ -1,10 +1,33 @@
 import { onRequest } from 'firebase-functions/v2/https';
-import * as logger from 'firebase-functions/logger';
+import admin from 'firebase-admin';
 
-export const createUser = onRequest((request, response) => {
+type RequestBody = {
+  phone: string;
+}
+
+export const createUser = onRequest(async (request, response) => {
   if (request.method === 'POST') {
-    logger.info('Hello logs!', { structuredData: true });
-    const { name } = request.body as { name: string; };
-    response.send(`Hello from Firebase ${name}!`);
+    const { phone } = request.body as RequestBody;
+
+    // Verify the user provided a phone
+    if (!phone) {
+      response.status(422).send({ error: 'Bad Input' });
+      return;
+    }
+
+    // Format the phone number to removed dashes and parens
+    const formattedPhone = String(phone).replace(/\D/g, '');
+
+    // Create the user account using the phone number
+    try {
+      const user = await admin.auth().createUser({ uid: formattedPhone });
+
+      // Respond to the user request, saying the account was made
+      response.send(user);
+    } catch (e) {
+      if (e instanceof Error) {
+        response.status(500).send({ error: e.message });
+      }
+    }
   }
 });
