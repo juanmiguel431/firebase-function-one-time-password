12118e8c27a426575af416c16840ea4bc5c2c55d
juanmiguel431 <juanmiguel431@gmail.com>
2023-10-08 13:01:15 -0400

    Setting a post request.

--- functions/src/createUser.ts
+++ functions/src/createUser.ts
@@ -2,6 +2,9 @@ import { onRequest } from 'firebase-functions/v2/https';
 import * as logger from 'firebase-functions/logger';
 
 export const createUser = onRequest((request, response) => {
-  logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase Juan Miguel Paulino Carpio!');
+  if (request.method === 'POST') {
+    logger.info('Hello logs!', { structuredData: true });
+    const { name } = request.body as { name: string; };
+    response.send(`Hello from Firebase ${name}!`);
+  }
 });
